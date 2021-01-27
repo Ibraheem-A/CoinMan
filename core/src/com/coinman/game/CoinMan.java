8d48df4aaab4f7ca5a7d70b28dc1036bf6d01433
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-01-27 22:28:40 +0100

    add man to screen and get man to gravitate

--- core/src/com/coinman/game/CoinMan.java
+++ core/src/com/coinman/game/CoinMan.java
@@ -2,20 +2,53 @@ package com.coinman.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
+	Texture background;
+	Texture[] man;
+	int manState = 0;
+	int pause = 0;
+	float gravity = 0.2f;
+	float velocity = 0f;
+	int manY = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+		man = new Texture[4];
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
+
+		manY = Gdx.graphics.getHeight() / 2;
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		if (pause < 8) {
+			pause++;
+		} else {
+			pause = 0;
+			if (manState < 3) {
+				manState++;
+			} else {
+				manState = 0;
+			}
+		}
+
+		velocity += gravity;
+		manY -= velocity;
+
+		batch.draw(man[manState], (float) (Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 2), (float) (manY - man[manState].getHeight() / 2));
+		batch.end();
 	}
 	
 	@Override
